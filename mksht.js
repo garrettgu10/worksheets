49c2cd2c3d63e7db51a948b56f07f2103acd9ecf
garrettgu10 <garrettgu777@gmail.com>
2016-09-24 15:44:48 -0500

    Separated dictionary and questions

--- mksht.js
+++ mksht.js
@@ -5,11 +5,11 @@ function makeSheet(){
     var nums = question.genNums();
     var answer = question.ans(nums);
     var text = question.text;
-    var wordTypes = question.items;
+    var wordTypes = question.wordTypes;
     for(var j = 0; j < nums.length; j++){
       text = text.replace(new RegExp("NUM"+String(j),'g'),String(nums[j]));
     }
-    for(var j = 0; j < question.items.length; j++){
+    for(var j = 0; j < question.wordTypes.length; j++){
       text = text.replace(new RegExp("WRD"+String(j),'g'),randomWord(wordTypes[j]));
     }
     document.getElementById("textArea").innerHTML += String(i+1)+ ". "+text+" <b>"+String(question.ans(nums))+"</b><br />";
---
+++ questions.js
@@ -0,0 +1,38 @@
+questions = [
+  {
+    text: "If you had NUM0 WRD2 WRD0s and you divided them among NUM1 WRD3 WRD1s, how many WRD2 WRD0s would each WRD3 WRD1 get?",
+    wordTypes: [noun,noun,adj,adj],
+    ans: function(nums){
+      return nums[0]/nums[1];
+    },
+    genNums: function(){
+      var two = parseInt(Math.floor(Math.random()*10+1));
+      var one = two*parseInt(Math.floor(Math.random()*10+1));
+      return [one, two];
+    }
+  },
+  {
+    text: "WRD3 has to WRD0 NUM0 WRD2 WRD1s. If he can WRD0 NUM1 WRD2 WRD1s in each hour, how many hours will it take for him to WRD0 all NUM0 WRD2 WRD1s?",
+    wordTypes: [verb,noun,adj,boyname],
+    ans: function(nums){
+      return nums[0]/nums[1];
+    },
+    genNums: function(){
+      var two = parseInt(Math.floor(Math.random()*10+1));
+      var one = two*parseInt(Math.floor(Math.random()*10+1));
+      return [one, two];
+    }
+  },
+  {
+    text: "How many WRD3 WRD0s are in a WRD5 WRD2 if there are NUM0 WRD3 WRD0s in a WRD4 WRD1 and NUM1 WRD4 WRD1s in a WRD5 WRD2?",
+    wordTypes: [noun,noun,noun,adj,adj,adj],
+    ans: function(nums){
+      return nums[0]*nums[1];
+    },
+    genNums: function(){
+      var two = parseInt(Math.floor(Math.random()*10+1));
+      var one = parseInt(Math.floor(Math.random()*10+1));
+      return [one, two];
+    }
+  }
+];
